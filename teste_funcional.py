62b1bbf6023a41a5772fce2686ec99c6a28a14e0
Caio Cezar de Assis Santos <ckaiocezar9@gmail.com>
2025-05-30 21:45:06 -0300

    Adiciona teste funcional básico

---
+++ teste_funcional.py
@@ -0,0 +1,11 @@
+﻿def main():
+    print(\"Teste de funcionamento do SmartBank AI:\")
+    saldo = 1000.0
+    saque = 200.0
+    saldo -= saque
+    print(f\"Saldo inicial: R$ 1000.00\")
+    print(f\"Saque efetuado: R$ {saque:.2f}\")
+    print(f\"Saldo atual: R$ {saldo:.2f}\")
+
+if __name__ == \"__main__\":
+    main()
