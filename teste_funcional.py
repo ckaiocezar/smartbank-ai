e23ce993692f05f02d3ae3d72bc5a0cad2e2eb57
Caio Cezar de Assis Santos <ckaiocezar9@gmail.com>
2025-05-30 21:47:19 -0300

    Adiciona teste funcional básico corrigido

--- teste_funcional.py
+++ teste_funcional.py
@@ -1,11 +1,11 @@
 ﻿def main():
-    print(\"Teste de funcionamento do SmartBank AI:\")
+    print('Teste de funcionamento do SmartBank AI:')
     saldo = 1000.0
     saque = 200.0
     saldo -= saque
-    print(f\"Saldo inicial: R$ 1000.00\")
-    print(f\"Saque efetuado: R$ {saque:.2f}\")
-    print(f\"Saldo atual: R$ {saldo:.2f}\")
+    print(f'Saldo inicial: R$ 1000.00')
+    print(f'Saque efetuado: R$ {saque:.2f}')
+    print(f'Saldo atual: R$ {saldo:.2f}')
 
-if __name__ == \"__main__\":
+if __name__ == '__main__':
     main()
